97129928dcd1d269528a08dd48bf23b69608651c
Armaan044 <deeparmaan070@gmail.com>
2024-09-26 11:09:48 -0400

    addedfile

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+import ("fmt")
+
+func main() {
+Greeting()
+advanceGreeting("Armaan")
+
+}
+
+
+func Greeting() {
+fmt.Println("Hey there!")
+
+}
+
+func advanceGreeting(name string){
+fmt.Printf(" Hello, %s!\n", Armaan)
+}
